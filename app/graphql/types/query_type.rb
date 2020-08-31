3575840c161b266a48001c8121a13f000ddfc4a7
Andrés Quilodrán <andres.caro.q@gmail.com>
2020-08-31 13:16:08 -0400

    Add error for when record is not found or invalid

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -12,6 +12,11 @@ module Types
 
     def user(id:)
       User.find(id)
+    rescue ActiveRecord::RecordNotFound => _e
+      GraphQL::ExecutionError.new('User does not exist.')
+    rescue ActiveRecord::RecordInvalid => e
+      GraphQL::ExecutionError.new("Invalid attributes for #{e.record.class}:"\
+        " #{e.record.errors.full_messages.join(', ')}")
     end
   end
 end
